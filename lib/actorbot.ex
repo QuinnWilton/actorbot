60a353aef6e804a3b07421042a469272b82a1bac
Shane Wilton <shane.wilton@gmail.com>
2017-02-28 21:37:37 -0800

    mix new actorbot

---
+++ lib/actorbot.ex
@@ -0,0 +1,18 @@
+defmodule Actorbot do
+  @moduledoc """
+  Documentation for Actorbot.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Actorbot.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/actorbot_test.exs
@@ -0,0 +1,8 @@
+defmodule ActorbotTest do
+  use ExUnit.Case
+  doctest Actorbot
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
