86ebcf9d7caea2f991a17377580a9b3cf90749c5
Shane Wilton <shane.wilton@gmail.com>
2017-02-28 22:17:52 -0800

    Configure everything to work with HashNuke/heroku-buildpack-elixir

--- config/config.exs
+++ config/config.exs
@@ -1,1 +1,3 @@
 use Mix.Config
+
+config :actorbot, :port, System.get_env("PORT")
--- lib/actorbot.ex
+++ lib/actorbot.ex
@@ -2,9 +2,13 @@ defmodule Actorbot do
   use Application
 
   def start(_type, _args) do
+    port =
+      Application.get_env(:actorbot, :port)
+      |> String.to_integer
+
     children = [
       Plug.Adapters.Cowboy.child_spec(:http, Actorbot.SlackRouter, [],
-        [port: 4000])
+        [port: port])
     ]
 
     opts = [strategy: :one_for_one, name: Actorbot.Supervisor]
